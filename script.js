695fe613829229d66f9fe6c99cfe59764bcbfd0f
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-11-15 18:55:32 +0400

    feat: add timer and counter

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
 // options
 let level = 16;
+let levelSqrt = Math.sqrt(level);
 // calc item size
 let itemSize = document.documentElement.clientWidth > document.documentElement.clientHeight
-    ? document.documentElement.clientHeight / (Math.sqrt(level)+1)
-    : document.documentElement.clientWidth / (Math.sqrt(level)+1);
+    ? document.documentElement.clientHeight / (levelSqrt + 1)
+    : document.documentElement.clientWidth  / (levelSqrt + 1);
+let count = 0;
+//create header
+createHeader = () => {
+    const header = document.createElement("header");
+    const timer = document.createElement("div");
+    const counter = document.createElement("div");
+    const btn = document.createElement("btn");
+    timer.className = "timer";
+    counter.className = "counter";
+    btn.className = "btn";
+    btn.textContent = "Pause"
+    document.body.append(header);
+    header.append(timer);
+    header.append(counter);
+    header.append(btn);
+    counter.innerHTML = `Moves: ${count}`
+    header.style.width = levelSqrt * itemSize + 'px';
+
+    let time = 0;
+    const upgradeTimer = () => {
+        time++
+        timer.innerHTML = `${Math.floor(time/60)} : ${time%60}`
+    }
+    let upgradeTimerInterval = setInterval(upgradeTimer, 1000)
+    
+    btn.onclick = (e) => {
+        e.target.parentElement.classList.toggle("bloked");
+        e.currentTarget.classList.toggle("active");
+        e.target.className.includes("active")
+        ? clearInterval(upgradeTimerInterval)
+        : upgradeTimerInterval = setInterval(upgradeTimer, 1000)
+    }
+}
+
+const writeCouter = count => {
+    document.querySelector(".counter").innerHTML = ` Moves: ${count}`
+}
 
 //create game field
 function createGameField (level) {
-    let levelSqrt = Math.sqrt(level);
+    
     const field = document.createElement("div");
     //save empty coordinates
     const emptyPosition = {
         top: levelSqrt - 1,
         left: levelSqrt - 1
     };
+    emptyPosition.id = "" + emptyPosition.left + emptyPosition.top;
     const ArrayItems = [];
     ArrayItems.push(emptyPosition);
     field.classList.add("field");
     document.body.append(field);
     //item size
+    field.style.fontSize = itemSize + "px";
     field.style.width = levelSqrt * itemSize + 'px';
     field.style.height = levelSqrt * itemSize + 'px';
     field.style.fontSize = itemSize + "px";
@@ -37,32 +77,38 @@ function createGameField (level) {
         ArrayItems.push({
             left: left,
             top: top,
-            el: item
+            el: item,
+            id: ""+left+top
         });
         // item width and height
         item.style.left = `${left * itemSize}px`;
         item.style.top = `${top * itemSize}px`;
         // click on item event listener
         item.addEventListener("click", (e) => {
-            console.log(e.preventDefault())
             e.preventDefault();
-            MakeAMove(i+1, ArrayItems, emptyPosition)
+            MakeAMove(i+1, ArrayItems, emptyPosition, 'clicked')
+            // after compelete
+            function finish () {
+                alert('done')
+            }
+            //check complete level and call func
+            if (ArrayItems.every(item => "" + item.left + item.top === item.id)) {
+                setTimeout(finish, 300);
+            }
         })
     }
 
-    // temp random
+    //temp random
     for (let i=0; i<20000*level; i++) {
          MakeAMove(Math.floor(Math.random() * (level)), ArrayItems, emptyPosition)
     }
-
 }
 
 // function click on item
-const MakeAMove = (index, ArrayItems, emptyPosition) => {
+const MakeAMove = (index, ArrayItems, emptyPosition, typeCall) => {
     const item = ArrayItems[index];
     // check valid click
     if (Math.abs(emptyPosition.left - item.left) + Math.abs(emptyPosition.top - item.top) === 1) {
-        // console.log('index')
         //coordinates mapping (emty and clicked item)
         item.el.style.left = `${emptyPosition.left * itemSize}px`;
         item.el.style.top = `${emptyPosition.top * itemSize}px`;
@@ -72,12 +118,11 @@ const MakeAMove = (index, ArrayItems, emptyPosition) => {
         emptyPosition.top = item.top;
         item.left = tempLeft;
         item.top = tempTop;
-    }
 
-    
+        typeCall ? writeCouter(++count) : "";
+    }
 }
 
-
-
 //call
+createHeader()
 createGameField(level);
